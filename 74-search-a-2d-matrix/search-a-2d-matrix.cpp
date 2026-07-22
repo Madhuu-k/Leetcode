628c6c9d7d89b4a1b24f4fe80f73b2e53c5b3431
K Madhu Sudhan Rao <2300031210@kluniversity.in>
2026-07-22 20:25:30 +0530

    Time: 0 ms (100.00%) | Memory: 13.5 MB (7.56%) - LeetSync

--- 74-search-a-2d-matrix/search-a-2d-matrix.cpp
+++ 74-search-a-2d-matrix/search-a-2d-matrix.cpp
@@ -1,20 +1,20 @@
 class Solution {
 public:
-    bool isThere(vector<int>& arr, int target){
-        int left = 0, right = arr.size() - 1;
-        bool res = false;
-        while(left <= right){
-            int mid = left + (right - left) / 2;
+    bool isPresent(vector<int>& arr, int target){
+        int low = 0;
+        int high = arr.size() - 1;
+        while(low <= high){
+            int mid = low + (high - low) / 2;
             if(arr[mid] == target) return true;
-            else if(arr[mid] > target) right = mid - 1;
-            else left = mid + 1;
+            if(arr[mid] < target) low = mid + 1;
+            else high = mid - 1;
         }
         return false;
     }
 
     bool searchMatrix(vector<vector<int>>& matrix, int target) {
         for(auto it : matrix){
-            if(isThere(it, target)) return true;
+            if(isPresent(it, target)) return true;
         }
         return false;
     }
