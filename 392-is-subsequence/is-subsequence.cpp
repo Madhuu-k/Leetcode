b69ea15616a8bb7f5b4a2ccbde411d6aa85df10f
K Madhu Sudhan Rao <2300031210@kluniversity.in>
2026-07-24 07:10:19 +0530

    Time: 0 ms (100.00%) | Memory: 8.5 MB (91.71%) - LeetSync

---
+++ 392-is-subsequence/is-subsequence.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    bool isSubsequence(string s, string t) {
+        int left = 0;
+        for(int right = 0; right < t.length(); right++){
+            if(s[left] == t[right]) left++;
+        }
+        return left == s.length();
+    }
+};
