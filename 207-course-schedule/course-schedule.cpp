d5984c94d03d93eb2303fc98c98a7ef635e2a2cf
K Madhu Sudhan Rao <2300031210@kluniversity.in>
2026-07-26 22:02:13 +0530

    Time: 8 ms (34.16%) | Memory: 19.3 MB (67.97%) - LeetSync

--- 207-course-schedule/course-schedule.cpp
+++ 207-course-schedule/course-schedule.cpp
@@ -1,28 +1,28 @@
 class Solution {
 public:
-    bool dfs(int node, vector<vector<int>>& adj, vector<bool>& visited, vector<bool>& resStack){
+    bool isValid(vector<vector<int>>& adj, vector<bool>& visited, vector<bool>& resStack, int node){
         visited[node] = true;
         resStack[node] = true;
 
         for(int neighbour : adj[node]){
-            if(!visited[neighbour] && dfs(neighbour, adj, visited, resStack)) return true;
+            if(!visited[neighbour] && isValid(adj, visited, resStack, neighbour)) return true;
             else if(resStack[neighbour]) return true;
         }
 
         resStack[node] = false;
-
         return false;
     }
 
-    bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
+    bool canFinish(int numCourses, vector<vector<int>>& preReq) {
         vector<vector<int>> adj(numCourses);
-        for(auto& e : prerequisites) adj[e[1]].push_back(e[0]);
-        
-        vector<bool> visited(numCourses, false);
+
         vector<bool> resStack(numCourses, false);
+        vector<bool> visited(numCourses, false);
+
+        for(auto& e : preReq) adj[e[1]].push_back(e[0]);
 
         for(int i = 0; i < numCourses; i++){
-            if(!visited[i] && dfs(i, adj, visited, resStack)) return false;
+            if(!visited[i] && isValid(adj, visited, resStack, i)) return false;
         }
 
         return true;
