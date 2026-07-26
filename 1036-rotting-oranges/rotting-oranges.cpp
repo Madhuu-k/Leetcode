9fd5bb0a042dd669b92b539e85f628bed900edef
K Madhu Sudhan Rao <2300031210@kluniversity.in>
2026-07-26 20:49:39 +0530

    Time: 0 ms (100.00%) | Memory: 16.8 MB (70.57%) - LeetSync

--- 1036-rotting-oranges/rotting-oranges.cpp
+++ 1036-rotting-oranges/rotting-oranges.cpp
@@ -4,26 +4,24 @@ public:
     int dc[4] = {0, 0, -1, 1};
 
     int orangesRotting(vector<vector<int>>& grid) {
-        queue<pair<int, int>> q;
         int rows = grid.size();
         int cols = grid[0].size();
 
+        queue<pair<int, int>> q;
         int fresh = 0;
 
         for(int i = 0; i < rows; i++){
             for(int j = 0; j < cols; j++){
                 if(grid[i][j] == 2) q.push({i, j});
-                else if(grid[i][j] == 1) fresh++; 
+                else if(grid[i][j] == 1) fresh++;
             }
-        }
+        }  
 
         int mins = 0;
-
         while(!q.empty() && fresh > 0){
             int size = q.size();
-
             while(size--){
-                auto [r, c] = q.front();
+                auto[r, c] = q.front();
                 q.pop();
 
                 for(int k = 0; k < 4; k++){
@@ -31,16 +29,14 @@ public:
                     int nc = c + dc[k];
 
                     if(nr >= 0 && nc >= 0 && nr < rows && nc < cols && grid[nr][nc] == 1){
-                        grid[nr][nc] = 2;
                         fresh--;
                         q.push({nr, nc});
+                        grid[nr][nc] = 2;
                     }
                 }
             }
-
             mins++;
-        }
-
-        return (fresh == 0) ? mins : -1;
+        }  
+        return(fresh == 0) ? mins : -1;
     }
 };
