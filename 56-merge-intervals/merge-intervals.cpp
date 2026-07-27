29b33b0c0ca476a734b610a1f395f9a639fdc1ca
K Madhu Sudhan Rao <2300031210@kluniversity.in>
2026-07-27 19:05:57 +0530

    Time: 12 ms (8.23%) | Memory: 25.8 MB (5.05%) - LeetSync

--- 56-merge-intervals/merge-intervals.cpp
+++ 56-merge-intervals/merge-intervals.cpp
@@ -4,18 +4,19 @@ public:
         vector<vector<int>> result;
         sort(intervals.begin(), intervals.end());
 
-        for(auto& it : intervals){
+        for(auto it : intervals){
             if(result.empty()){
                 result.push_back(it);
                 continue;
             }
 
-            vector<int> lastIndex = result.back();
-            if(lastIndex[1] >= it[0]){
-                result.back()[1] = max(lastIndex[1], it[1]);
+            vector<int> lastInterval = result.back();
+            if(lastInterval[1] >= it[0]){
+                result.back()[1] = max(lastInterval[1], it[1]);
             }
             else result.push_back(it);
         }
+
         return result;
     }
 };
