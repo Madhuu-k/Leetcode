e81f9ab1254cd8fb11cc36e71f4dd33190a19828
K Madhu Sudhan Rao <2300031210@kluniversity.in>
2026-08-30 13:03:35 +0530

    Time: 4 ms (40.44%) | Memory: 13.8 MB (71.00%) - LeetSync

--- 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.py
+++ 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.py
@@ -5,9 +5,10 @@ class Solution(object):
         :rtype: int
         """
         left = 0
-        for right in range(len(nums)):
+        for right in range(1, len(nums)):
             if nums[left] != nums[right]:
                 left += 1
                 nums[left] = nums[right]
 
         return left + 1
+        
