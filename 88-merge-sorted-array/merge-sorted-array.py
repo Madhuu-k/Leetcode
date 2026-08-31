4951288f69dbbc2531957766206825da75640566
K Madhu Sudhan Rao <2300031210@kluniversity.in>
2026-08-31 20:28:09 +0530

    Time: 0 ms (100.00%) | Memory: 12.3 MB (57.81%) - LeetSync

--- 88-merge-sorted-array/merge-sorted-array.py
+++ 88-merge-sorted-array/merge-sorted-array.py
@@ -1,5 +1,12 @@
 class Solution(object):
     def merge(self, nums1, m, nums2, n):
+        """
+        :type nums1: List[int]
+        :type m: int
+        :type nums2: List[int]
+        :type n: int
+        :rtype: None Do not return anything, modify nums1 in-place instead.
+        """
         i = m - 1
         j = n - 1
         k = m + n - 1
@@ -13,10 +20,8 @@ class Solution(object):
                 i -= 1
             
             k -= 1
-        
+
         while j >= 0:
             nums1[k] = nums2[j]
             j -= 1
             k -= 1
-
-
