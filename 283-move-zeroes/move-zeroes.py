0730981dbab3bcb2143f3f446860a90ab5268fe0
K Madhu Sudhan Rao <2300031210@kluniversity.in>
2026-09-19 11:36:56 +0530

    Time: 6 ms (62.10%) | Memory: 13.6 MB (20.30%) - LeetSync

---
+++ 283-move-zeroes/move-zeroes.py
@@ -0,0 +1,11 @@
+class Solution(object):
+    def moveZeroes(self, nums):
+        pos = 0
+        for i in range(len(nums)):
+            if nums[i] != 0:
+                nums[i], nums[pos] = nums[pos], nums[i]
+                pos += 1
+
+        return nums
+
+        
